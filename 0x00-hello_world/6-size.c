a69dff7bb48f4a85866ee6af6e0ec21120ff9c91
ggirlk <ggirlk2@gmail.com>
2019-09-18 17:37:39 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,12 @@
  * prints the different types sizes
  * Return: Always 0 (Success)
  */
-char s;
-int d;
-long int li;
-long long int lli;
-float f;
 int main(void)
 {
-printf("Size of char: %d byte(s)\n", sizeof(s));
-printf("Size of int: %d byte(s)\n", sizeof(d));
-printf("Size of long int: %d byte(s)\n", sizeof(li));
-printf("Size of long long int: %d byte(s)\n", sizeof(lli));
-printf("Size of float: %d byte(s)\n", sizeof(f));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
