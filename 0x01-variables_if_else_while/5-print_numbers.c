07e79c70f065072c1c715e7327497e2d0d5eab47
ggirlk <ggirlk2@gmail.com>
2019-09-19 19:50:31 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char n = 0;
+while (n < 10)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
