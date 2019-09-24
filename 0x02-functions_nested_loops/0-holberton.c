38e77f65f1d81fe95e7b8631378478abd87d645b
ggirlk <ggirlk2@gmail.com>
2019-09-24 09:29:15 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
-
 int _putchar(char c);
 
 int main(void)
 {
-_putchar(72);
-_putchar(111);
-_putchar(108);
-_putchar(98);
-_putchar(101);
-_putchar(114);
-_putchar(116);
-_putchar(111);
-_putchar(110);
-_putchar(10);
+char ch[9];
+int i = 0;
+ch[0] = 'H';
+ch[1] = 'o';
+ch[2] = 'l';
+ch[3] = 'b';
+ch[4] = 'e';
+ch[5] = 'r';
+ch[6] = 't';
+ch[7] = 'o';
+ch[8] = 'n';
+ch[9] = '\n';
+while(i <= 9)
+{
+_putchar(ch[i]);
+i++;
+}
 return (0);
 }
