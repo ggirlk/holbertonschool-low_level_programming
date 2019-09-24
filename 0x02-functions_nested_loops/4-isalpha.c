41e4556185ac1614e8b4b3f128735c01bec9aa4a
ggirlk <ggirlk2@gmail.com>
2019-09-24 11:27:45 +0000

    yes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+/**
+ * function _isalpha.
+ *
+ * Return: 1 if variable is a lowercase character and 0 if not
+ */
+int _isalpha(char c)
+{
+char str;
+int ret = 0;
+for (str = 'a'; str <= 'z'; str++)
+{
+if (c == str)
+{
+ret = 1;
+}
+}
+for (str = 'A'; str <= 'Z'; str++)
+{
+if (c == str)
+{
+ret = 1;
+}
+}
+return (ret);
+}
