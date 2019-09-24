0319e1b4b9615d59ee432ecdc9e741cbbb3194b8
ggirlk <ggirlk2@gmail.com>
2019-09-24 17:51:13 +0000

    yes

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,16 @@
 #include"putchar.h"
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char ch[] = "Holberton\n";
 int i = 0;
-while(i <= 9)
+for (i = 0; i <= 9; i++)
 {
 _putchar(ch[i]);
-i++;
 }
 return (0);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,18 +6,30 @@
  */
 void times_table(void)
 {
-int i, j, k;
+int i, j, m, k, l;
 for (i = 0; i <= 9 ; i++)
 {
-	for (j = 0; j <= 9 ; j++)
-	{
-		k = i * j;
-		_putchar('0' + k);
-		if (k == 81)
-		break;
-		_putchar(',');
-		_putchar(' ');
-	}
-	_putchar('\n');
+for (j = 0; j <= 9 ; j++)
+{
+m = i * j;
+if (m >= 10)
+{
+k = m / 10;
+l = m % 10;
+_putchar('0' + k);
+_putchar('0' + l);
+} else
+{
+_putchar('0' + m);
+}
+if (j == 9)
+{
+break;
+}
+
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
 }
 }
