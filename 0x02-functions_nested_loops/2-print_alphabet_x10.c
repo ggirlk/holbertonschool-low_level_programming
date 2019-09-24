f70938568ee879c6e64244b66a599ec68078aab3
ggirlk <ggirlk2@gmail.com>
2019-09-24 10:30:08 +0000

    yes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
-
+/**
+ * prints alphabets 10 times
+ */
 void print_alphabet_x10(void)
 {
 char str;
 int i;
 for (i = 0; i < 10; i++) {
-for (str = 'a'; str < 'z'; str++) {
+for (str = 'a'; str <= 'z'; str++) {
 _putchar(str);
 }
 _putchar('\n');
