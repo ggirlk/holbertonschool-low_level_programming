5550d942e4ac7fbc21d7867892723ea148d875f7
ggirlk <ggirlk2@gmail.com>
2019-09-24 10:34:40 +0000

    yes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,11 @@ void print_alphabet_x10(void)
 {
 char str;
 int i;
-for (i = 0; i < 10; i++) {
-for (str = 'a'; str <= 'z'; str++) {
+for (i = 0; i < 20; i++)
+{
+for (str = 'a'; str <= 'z'; str++)
 _putchar(str);
-}
+
 _putchar('\n');
 i++;
 }
