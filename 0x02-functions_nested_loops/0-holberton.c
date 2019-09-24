0dbd76820551b9e8afff24292296752454700e6e
ggirlk <ggirlk2@gmail.com>
2019-09-24 09:38:43 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,18 +2,8 @@
 
 int main(void)
 {
-char ch[9];
+char ch[] = "Holberton\n";
 int i = 0;
-ch[0] = 'H';
-ch[1] = 'o';
-ch[2] = 'l';
-ch[3] = 'b';
-ch[4] = 'e';
-ch[5] = 'r';
-ch[6] = 't';
-ch[7] = 'o';
-ch[8] = 'n';
-ch[9] = '\n';
 while(i <= 9)
 {
 _putchar(ch[i]);
