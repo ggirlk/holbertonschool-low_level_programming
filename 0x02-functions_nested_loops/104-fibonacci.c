4f48afeb1b4ffc11a182f5cb09f02db5c31e8317
ggirlk <ggirlk2@gmail.com>
2019-09-25 16:33:18 +0000

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,24 @@
  */
 int main(void)
 {
-int first = 0, second = 1, next, c;
+unsigned long first = 0, second = 1, next, c, part1, part2;
 for (c = 1; c <= 98; c++)
-{
-if (c <= 2)
-next = c;
-else
 {
 next = first + second;
 first = second;
 second = next;
+
+if (sizeof(next) <= 4)
+{
+printf("%lu", next);
 }
-printf("%d", next);
+else
+{
+part1 = next / 100000000000;
+part2 = next % 1000000000000;
+printf("%lu", part1);
+printf("%ld", part2);
+}
 if (c == 98)
 break;
 
