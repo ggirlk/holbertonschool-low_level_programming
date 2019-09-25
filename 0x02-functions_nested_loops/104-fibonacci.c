0b759e644f20a805dac8fef78fdded006ad15670
ggirlk <ggirlk2@gmail.com>
2019-09-25 15:34:06 +0000

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,7 @@
  *
  *Return: 0
  */
-int main (void)
+int main(void)
 {
 int first = 0, second = 1, next, c;
 for (c = 1; c <= 98; c++)
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,30 @@ void print_to_98(int n)
 {
 if(n < 0)
 {
-int i;
+int i, x, y, z;
 for (i = 0; i >= n; i--)
 {
-if (i > 10
+if (i >= 10)
+{
+x = i / 10;
+y = i % 10;
+_putchar('0' +x);
+_putchar('0' +y);
+}
+else if (i >= 100)
+{
+x = (i / 10) % 10;
+y = i % 10;
+z = i / 100;
+_putchar('0' +x);
+_putchar('0' +y);
+_putchar('0' +z);
+}
+else
+{
+_putchar('0' + i);
+}
+
 }
 }
 }
