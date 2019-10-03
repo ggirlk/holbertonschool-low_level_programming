9ebc0c9830220f578221d9fd648860e3973d4b21
ggirlk <ggirlk2@gmail.com>
2019-10-03 11:08:20 +0000

    strncopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+int i = 0;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-   *(dest + i) = *(src + i);
+while (i < n && src[i] != '\0')
+{
+*(dest + i) = *(src + i);
+i++;
+}
 
 while (i < n)
 {
