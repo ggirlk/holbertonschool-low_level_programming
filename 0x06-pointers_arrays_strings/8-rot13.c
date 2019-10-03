63624a3f958d3424866fc289af68efa051bd6596
ggirlk <ggirlk2@gmail.com>
2019-10-03 22:56:32 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string to encode.
+ * Return: char.
+ */
+char *rot13(char *str)
+{
+int i = 0, j, k = 0;
+char al1[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+char al2[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+
+while (str[i])
+{
+    j = 0;
+while (al1[j] && k == 0)
+{
+if (str[i] == al1[j])
+{
+str[i] = al2[j];
+k = 1;
+}
+j++;
+}
+k = 0;
+i++;
+}
+return (str);
+}
