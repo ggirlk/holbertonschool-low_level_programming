1e648adf9b56092e8446299c45d52f464a02fa9e
ggirlk <ggirlk2@gmail.com>
2019-10-03 19:14:16 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337.
+ * @str: string to encode.
+ * Return: char.
+ */
+char *leet(char *str)
+{
+int i = 0;
+//char a, b;
+while (str[i])
+{
+/*a = 'a';
+b = 'A';*/
+
+if (str[i] == 'a' || str[i] == 'A')
+{
+str[i] = '4';
+}
+else if (str[i] == 'e' || str[i] == 'E')
+{
+str[i] = '3';
+}
+else if (str[i] == 'o' || str[i] == 'O')
+{
+str[i] = '0';
+}
+else if (str[i] == 't' || str[i] == 'T')
+{
+str[i] = '7';
+}
+else if (str[i] == 'l' || str[i] == 'L')
+{
+str[i] = '1';
+}
+i++;
+}
+return (str);
+}
