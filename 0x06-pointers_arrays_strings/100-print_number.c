f9f5d44f2419e3a881918f54193f20523d01f685
ggirlk <ggirlk2@gmail.com>
2019-10-04 18:35:36 +0000

    numbers

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -6,61 +6,34 @@
  */
 void print_number(int n)
 {
-int k, j, l, m, h = 0;
+int k, j, l, m;
 if (n < 0)
 {
-h = 1;
+_putchar('-');
 n = -1 * n;
 }
-if (n > 10 && n < 100)
+if (n >= 1000)
+{
+m = n / 1000;
+m = m % 10;
+_putchar('0' + m);
+}
+if (n >= 100)
+{
+l = n / 100;
+l = l % 10;
+_putchar('0' + l);
+}
+if (n >= 10)
 {
 k = n % 10;
 j = n / 10;
-if (h == 1)
-{
-_putchar('-');
-}
+j = j % 10;
 _putchar('0' + j);
 _putchar('0' + k);
 }
-else if (n >= 100 && n < 1000)
+if (n < 10)
 {
-l = n / 100;
-k = n % 10;
-j = n / 10 - l * 10;
-if (h == 1)
-{
-_putchar('-');
-}
-_putchar('0' + l);
-_putchar('0' + j);
-_putchar('0' + k);
-}
-else if (n >= 1000 && n < 10000)
-{
-k = n % 10;
-n = n / 10;
-j = n % 10;
-n = n / 10;
-l = n % 10;
-n = n / 10;
-m = n % 10;
-
-if (h == 1)
-{
-_putchar('-');
-}
-_putchar('0' + m);
-_putchar('0' + l);
-_putchar('0' + j);
-_putchar('0' + k);
-}
-else
-{
-if (h == 1)
-{
-_putchar('-');
-}
 _putchar('0' + n);
 }
 }
