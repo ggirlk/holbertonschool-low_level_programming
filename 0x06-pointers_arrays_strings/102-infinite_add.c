f5ce6c57a0fddeac3edf42df9b2e1763d11853bc
ggirlk <ggirlk2@gmail.com>
2019-10-04 22:36:25 +0000

    magic

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two numbers.
+ * @n1: number
+ * @n2: number
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: pointer to the result.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
