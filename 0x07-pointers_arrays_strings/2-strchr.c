6c6adca559d34952317de6c873d1effa0b20ba2d
ggirlk <ggirlk2@gmail.com>
2019-10-07 19:26:11 +0000

    request

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,19 +10,17 @@
  */
 char *_strchr(char *s, char c)
 {
-int k = 0;
 while (*s)
 {
 if (*s == c)
 {
-k = 1;
 return (s);
 }
 s++;
 }
-if (k == 0)
+if (*s != c)
 {
-return ("NULL");
+return (NULL);
 }
 return ('\0');
 }
