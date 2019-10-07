045c9a89456949cc42d9c7f5f11e2633d27280e2
ggirlk <ggirlk2@gmail.com>
2019-10-07 21:28:58 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,30 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i = 0, j = 0, k;
+while (haystack[i])
+{
+k = 0;
 
+while (needle[j])
+{
+if (haystack[i] == needle[j])
+{
+k = 1;
+j++;
+}
+else
+{
+break;
+}
+}
+
+if (k == 1)
+return (haystack + i);
+i++;
+}
+
+if (k == 0)
+return (NULL);
 
 return ('\0');
 }
