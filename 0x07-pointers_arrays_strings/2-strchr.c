3d7f12ccb3f268d64d4b35ce0fa08fccfb2dcc75
ggirlk <ggirlk2@gmail.com>
2019-10-07 15:32:59 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string given
+ * @c: searchen character
+ *
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i = 0;
+char *ch;
+
+while (s[i])
+{
+if (s[i] == c)
+{
+ch = &s[i - 1];
+}
+i++;
+}
+
+return (ch);
+
+}
