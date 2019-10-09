73d4a707d97c78fbfb1d916901732f555261a9a3
ggirlk <ggirlk2@gmail.com>
2019-10-09 18:50:22 +0000

    request

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-int  i = 0;
-_putchar(*(s + i));
-if (*(s + i) != '\0')
-{
-i++;
-_puts_recursion(s + i);
-}
-else
+if (*s == '\0')
 {
 _putchar('\n');
+return;
 }
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
