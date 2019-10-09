6a4a3e8ab2824362d80998b93a55dd4964397642
ggirlk <ggirlk2@gmail.com>
2019-10-09 23:00:19 +0000

    request

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,13 +11,20 @@ int is_prime_number(int n)
 {
 return (primeCounter(2, n));
 }
-
+/**
+ * primeCounter - test if it is primary or no
+ * @k: counter
+ * @n number
+ * Return: int
+ */
 int primeCounter(int k, int n)
 {
 if (k < n && n % k == 0)
 return (0);
 else if (k < n && n % k != 0)
 return (1);
+else if (n == 0)
+return (-1);
 else
 {
 primeCounter(2 + 1, n);
