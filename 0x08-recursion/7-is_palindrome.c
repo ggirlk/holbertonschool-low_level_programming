33cb8cf0226c5a683f594f0d4cab37f6053fc0bd
ggirlk <ggirlk2@gmail.com>
2019-10-10 21:46:04 +0000

    request

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -4,9 +4,32 @@
 /**
  * is_palindrome - test if is a palindrome.
  * @s: string
- * Return: int.
+ * Return: int 0 or 1.
  */
 int is_palindrome(char *s)
 {
+int l = 0;
+while (*(s + l) != '\0')
+l++;
+return (palindromeCounter(l - 1, 0,s));
+
+return (0);
+}
+/**
+ * palindromeCounter - count and test chars.
+ * @s: string
+ * @l: length
+ * @i: counter
+ * Return: int 0 or 1.
+ */
+int palindromeCounter(int l, int i, char *s)
+{
+if (i == l)
+return (1);
+if (l == 1)
+return (1);
+if (*(s + l) == *(s + i))
+return (palindromeCounter(l - 1,i + 1,s));
+
 return (0);
 }
