af5c8c03ccc0a40734eaabf08d1baf2bc59b2d54
ggirlk <ggirlk2@gmail.com>
2019-10-16 13:33:11 +0000

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,12 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *arr = malloc(sizeof(char) * size);
-if (size == 0)
+if (size == 0 || arr == NULL)
 return (NULL);
 
 for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }
-if (arr)
-return (arr);
-else
-return (NULL);
+return arr;
 }
