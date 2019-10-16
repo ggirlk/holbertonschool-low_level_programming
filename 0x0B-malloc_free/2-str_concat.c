df5e4927fb8a33c1432bc35a51de2be78591b17d
ggirlk <ggirlk2@gmail.com>
2019-10-16 14:14:07 +0000

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,23 +4,35 @@
 
 /**
  * _strdup - returns a pointer to a newly allocated space in memory;
- * @str: string
+ * @s1: 1st string
+ * @s2: 2nd string
  * Return: pointer to a new string;.
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-int i, l;
+int i, l, l2, j = 0;
 char *nstr;
-if (str == NULL)
+if (s1 == NULL && s2 == NULL)
 return (NULL);
 
-l = _strlen_recursion(str);
-nstr = malloc(sizeof(char) * l);
+l = _strlen_recursion(s1);
+l2 = _strlen_recursion(s2);
+
+nstr = malloc(sizeof(char) * (l + l2));
 if (nstr == NULL)
 return (NULL);
 
 for (i = 0; i < l; i++)
-nstr[i] = str[i];
+{
+nstr[j] = s1[i];
+j++;
+}
+for (i = 0; i < l2; i++)
+{
+nstr[j] = s2[i];
+j++;
+}
+nstr[j] = '\0';
 
 return (nstr);
 
