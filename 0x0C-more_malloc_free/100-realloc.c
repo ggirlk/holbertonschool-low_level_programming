5427195dafd20359c4565938c0fdba208b1fb5ec
ggirlk <ggirlk2@gmail.com>
2019-10-17 19:57:45 +0000

    #advanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,8 +24,13 @@ return (NULL);
 }
 return (ptr2);
 }
-if(new_size == old_size)
+if (new_size == old_size)
 return (ptr);
+if (new_size == 0 && ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
 ptr2 = malloc(new_size);
 if (ptr2 == NULL)
 return (NULL);
