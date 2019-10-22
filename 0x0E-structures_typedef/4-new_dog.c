470ab0834b62f2eb692ce8eca6572f0585f6c23b
ggirlk <ggirlk2@gmail.com>
2019-10-22 20:55:43 +0000

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -27,8 +26,8 @@ if (name == NULL || owner == NULL || age <= 0)
 free(my_new_dog);
 return (NULL);
 }
-name1 = malloc(sizeof(char) * (strlen(name) + 1));
-owner1 = malloc(sizeof(char) * (strlen(owner) + 1));
+name1 = malloc(sizeof(char) * (_strlen(name) + 1));
+owner1 = malloc(sizeof(char) * (_strlen(owner) + 1));
 if (name1 == NULL || owner1 == NULL)
 {
 free(name1);
@@ -48,4 +47,18 @@ my_new_dog->name = name1;
 my_new_dog->owner = owner1;
 
 return (my_new_dog);
+}
+/**
+ * _strlen - count sting length.
+ * @s: string
+ * Return: string length.
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+{
+i++;
+}
+return (i);
 }
