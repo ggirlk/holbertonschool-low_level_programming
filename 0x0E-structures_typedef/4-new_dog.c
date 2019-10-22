e163d2fc65d97e09afbab4fe6a26c89ff46db784
ggirlk <ggirlk2@gmail.com>
2019-10-22 12:51:41 +0000

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,26 +11,14 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *my_new_dog = malloc(sizeof(struct dog));
-
+struct dog *my_new_dog;
+my_new_dog = malloc(sizeof(struct dog));
 if (my_new_dog == NULL)
 return (NULL);
-my_new_dog->name = malloc(sizeof(name));
-if (my_new_dog->name == NULL)
-{
-free(my_new_dog->name);
-free(my_new_dog);
-return (NULL);
-}
+
 my_new_dog->name = name;
 my_new_dog->age = age;
-my_new_dog->owner = malloc(sizeof(owner));
-if (my_new_dog->owner == NULL)
-{
-free(my_new_dog->owner);
-free(my_new_dog);
-return (NULL);
-}
 my_new_dog->owner = owner;
+
 return (my_new_dog);
 }
