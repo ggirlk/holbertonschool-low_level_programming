c236dfdf0058fd6d9f541a6fdadf7e0cc00af4c8
ggirlk <ggirlk2@gmail.com>
2019-10-22 12:35:42 +0000

    correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,10 +14,9 @@ if (d->name == NULL)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
-if (d->age == 0)
-printf("Age: (nil)\n");
-else
+
 printf("age: %f\n", d->age);
+
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
