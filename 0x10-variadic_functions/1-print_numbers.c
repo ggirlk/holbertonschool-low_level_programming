94642c269cc384dce82685089ac528af072cd966
ggirlk <ggirlk2@gmail.com>
2019-10-26 12:41:17 +0000

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 va_list args;
-if (n == 0)
+if (n == 0 || separator == NULL)
 return;
 va_start(args, n);
 for (i = 0; i < n; i++)
