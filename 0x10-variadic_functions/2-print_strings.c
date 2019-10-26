9609b0643c86bdc0dc2747c5af8b4cdc6e21f556
ggirlk <ggirlk2@gmail.com>
2019-10-26 14:28:23 +0000

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,6 +13,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list args;
 const char *str = NULL;
+if (n == 0)
+return;
 if (separator == NULL)
 separator = "";
 va_start(args, n);
