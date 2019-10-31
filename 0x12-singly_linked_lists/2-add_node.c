3842ff59b5b27c58bf375a431b6bda22e917f500
ggirlk <ggirlk2@gmail.com>
2019-10-31 20:48:37 +0000

    3rd spooky

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list
+ * Return: number of nodes.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *h;
+h = malloc(sizeof(list_t));
+if (h == NULL)
+return (NULL);
+h->len = _strlen((char *)str);
+h->str = (char *)str;
+h->next = *head;
+*head = h;
+return (h);
+}
+/**
+ * _strlen - count sting length.
+ * @s: string
+ * Return: string length.
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+{
+i++;
+}
+return (i);
+}
