70b4b195b9ed2067558d5afe9ba5cde0666c1597
ggirlk <ggirlk2@gmail.com>
2019-10-31 22:01:04 +0000

    4th spooky

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,22 +4,31 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning
+ * add_node_end - adds a new node at the end.
  * @head: list
  * @str: string
  * Return: node.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *h;
+list_t *h2;
 char *s = strdup(str);
 h = malloc(sizeof(list_t));
 if (h == NULL)
 return (NULL);
 h->len = _strlen(s);
 h->str = s;
-h->next = *head;
+h->next = NULL;
+if (*head == NULL)
 *head = h;
+else
+{
+h2 = *head;
+while (h2->next != NULL)
+h2 = h2->next;
+h2->next = h;
+}
 return (h);
 }
 /**
