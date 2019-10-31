0c7ebcee1ff2970093264c57381765d78524ac6e
ggirlk <ggirlk2@gmail.com>
2019-10-31 22:34:48 +0000

    add spooky

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,6 +13,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *h;
 char *s = strdup(str);
+if (str == NULL)
+return (NULL);
 h = malloc(sizeof(list_t));
 if (h == NULL)
 return (NULL);
