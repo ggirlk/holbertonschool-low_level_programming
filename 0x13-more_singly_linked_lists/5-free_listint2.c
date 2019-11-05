e548cf29147596d42aa4133d00ba17323f926546
ggirlk <ggirlk2@gmail.com>
2019-11-05 19:36:41 +0000

    free list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head: list
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *h;
+while (h != NULL)
+{
+h = *head;
+*head = h->next;
+free(h);
+}
+free(head);
+}
