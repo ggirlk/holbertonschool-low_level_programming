18dc2c7cecabfe8daf6ecfe00fdec6892304133b
ggirlk <ggirlk2@gmail.com>
2019-11-06 10:16:54 +0000

    betty check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
 /**
  * get_nodeint_at_index - returns the nth node of a list
  * @head: list
- *
+ * @index: index of the node
  * Return: int.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
