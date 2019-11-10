e2871a5c5c2c34ebaf76e61e4c2a40108b6130c4
ggirlk <ggirlk2@gmail.com>
2019-11-10 12:03:34 +0000

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,16 +9,21 @@
  */
 void print_binary(unsigned long int n)
 {
-int i = 32, k = 0;
+int i = 32, k = 0, tst = 0;
 if (n == 0)
 _putchar('0');
 while (i--)
 {
 k = n >> i;
 if (k & 1)
+tst = 1;
+if (tst == 1)
+{
+if (k & 1)
 _putchar('1');
 else
 _putchar('0');
+}
 }
 }
 
