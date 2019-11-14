0241e0bf50ebab1d77937e21d80f8a4f19a7010d
ggirlk <ggirlk2@gmail.com>
2019-11-14 23:48:56 +0000

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,21 +15,21 @@ int create_file(const char *filename, char *text_content)
 int f, k;
 size_t i = 0;
 char s;
+mode_t mode = S_IRUSR | S_IWUSR;
 if (filename == NULL)
 return (-1);
 if (text_content == NULL)
 return(1);
-f = open(filename, O_RDWR | O_CREAT | O_APPEND);
+f = open(filename, O_RDWR | O_CREAT | O_TRUNC, mode);
 if (f == -1)
-return (0);
-for (i = 0; text_content[i] != '\0';)
+return (-1);
+for (i = 0; text_content[i] != '\0'; i++)
 {
-i++;
-}
-k = write(STDOUT_FILENO, &s, i);
+s = text_content[i];
+k = dprintf(f, "%c", s);
 if (k == -1)
 return (-1);
-i++;
+}
 f = close(f);
 return (1);
 }
