7aeb702e22d71f25557e70ca9e965d8b32302a9c
ggirlk <ggirlk2@gmail.com>
2019-11-15 00:41:43 +0000

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
 #include "holberton.h"
 
 /**
- * create_file _ creates a file.
+ * create_file - creates a file.
  * @filename: file name
  * @text_content: NULL terminated string to write to the file
  * Return: int
@@ -15,12 +15,12 @@ int create_file(const char *filename, char *text_content)
 int f, k;
 size_t i = 0;
 char s;
-mode_t mode = S_IRUSR | S_IWUSR;
+/*mode_t mode = S_IRUSR | S_IWUSR;*/
 if (filename == NULL)
 return (-1);
 if (text_content == NULL)
-return(1);
-f = open(filename, O_RDWR | O_CREAT | O_TRUNC, mode);
+return (1);
+f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (f == -1)
 return (-1);
 for (i = 0; text_content[i] != '\0'; i++)
