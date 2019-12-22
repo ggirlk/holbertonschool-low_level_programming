9bd4209d36cb2970d4d199f70767cab17383fa9f
ggirlk <ggirlk2@gmail.com>
2019-12-22 02:58:55 +0000

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,12 +5,15 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: head
+ * @idx: position
+ * @n: data
  *
- * Return: Always EXIT_SUCCESS.
+ * Return: pointer to list.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int i = 0;
+unsigned int i = 0, k = 0;
 dlistint_t *nh = *h;
 if (idx < i || !(*h))
 return (NULL);
@@ -18,11 +21,15 @@ return (NULL);
 while (nh != NULL)
 {
 if (i == idx)
+{
 add_dnodeint_end(&(*h), n);
-
+k = 1;
+}
 add_dnodeint_end(&(*h), nh->n);
 nh = nh->next;
 i++;
 }
+if (k == 0)
+return (NULL);
 return (*h);
 }
