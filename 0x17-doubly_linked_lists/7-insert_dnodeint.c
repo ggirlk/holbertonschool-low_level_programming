3b35eddb4e843632c616a2ef3fd0fd7b3a420b8a
ggirlk <ggirlk2@gmail.com>
2019-12-22 02:55:36 +0000

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int i = 0;
 dlistint_t *nh = *h;
-/*nh = malloc(sizeof(dlistint_t));*/
+if (idx < i || !(*h))
+return (NULL);
 *h = NULL;
 while (nh != NULL)
 {
