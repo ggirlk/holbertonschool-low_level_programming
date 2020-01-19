3f532fc5fdc3cf9660acf62fd407a352300d5cd4
ggirlk <ggirlk2@gmail.com>
2020-01-19 19:01:22 +0000

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the array
+ * Return: a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *arr;
+arr = malloc(sizeof(hash_table_t));
+if (arr == NULL)
+{
+return (NULL);
+}
+arr->size = size;
+arr->array = NULL;
+return (arr);
+}
