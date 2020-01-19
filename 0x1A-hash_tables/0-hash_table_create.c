452c5dc8254bb6cefebafc757ee34ceb3ab9e037
ggirlk <ggirlk2@gmail.com>
2020-01-19 19:15:31 +0000

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *arr;
 arr = malloc(sizeof(hash_table_t));
-if (arr == NULL && size == 0)
+if (arr == NULL || size == 0)
 {
 return (NULL);
 }
