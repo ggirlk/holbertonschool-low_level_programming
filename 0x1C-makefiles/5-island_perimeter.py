e06a4bc2b3182de997786dcfdcf33926fbd66c0d
ggirlk <ggirlk2@gmail.com>
2020-02-21 23:08:16 +0000

    :')

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,8 +2,8 @@
 def island_perimeter(grid):
     p = 0
     l = len(grid) - 1
-    for i in (0, l):
-        for j in (0, len(grid[i]) - 1):
+    for i in range(0, l):
+        for j in range(0, len(grid[i]) - 1):
             if grid[i][j] == 1:
                 if i != 0 and grid[i][j - 1] == 0:
                     p += 1
