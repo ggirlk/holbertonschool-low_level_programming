ee9c8711bf6920d0cb0e321e93e39e259ccc9816
ggirlk <ggirlk2@gmail.com>
2020-04-22 17:46:56 +0000

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Linear search
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+size_t i;
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef SA_H
+#define SA_H
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* search algos */
